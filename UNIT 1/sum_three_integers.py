684c7307c728b5c8bf7689f88f53e89c62804170
Shivam Moraniya <shivamdmoraniya@gmail.com>
2026-04-02 15:45:25 +0530

    Add script to sum three integers from user input

---
+++ UNIT 1/sum_three_integers.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+
+sum_result = num1 + num2 + num3
+
+print(f"The sum of the three integers is: {sum_result}")
